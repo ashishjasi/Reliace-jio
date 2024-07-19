c2cc9b1bc6501d65806ca0426fb5c9f56ffb47d6
Nilamber <nilambersingh@gmail.com>
2024-07-19 16:14:19 +0530

    show my code

---
+++ Jiowrokshop2024/src/test/java/Employee/ToCreateEmployee.java
@@ -0,0 +1,10 @@
+package Employee;
+
+public class ToCreateEmployee {
+
+	public static void main(String[] args) {
+		System.out.println("Employee working 1M");
+		System.out.println("One Milion working per day");
+	}
+
+}
---
+++ Jiowrokshop2024/src/test/java/Manager/ToCreateMager.java
@@ -0,0 +1,9 @@
+package Manager;
+
+public class ToCreateMager {
+
+	public static void main(String[] args) {
+		System.out.println("JIO working manager 1K");
+		System.out.println("all Employee Manager");
+	}
+}
